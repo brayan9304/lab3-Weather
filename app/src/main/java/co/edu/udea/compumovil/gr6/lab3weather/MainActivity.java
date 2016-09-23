29257f4fb585f850f3ef0e34600e3aee38153149
jaime <jaime.jimenezisi@gmail.com>
2016-09-23 16:13:38 -0500

    Creación service para cargar el clima

--- app/src/main/java/co/edu/udea/compumovil/gr6/lab3weather/MainActivity.java
+++ app/src/main/java/co/edu/udea/compumovil/gr6/lab3weather/MainActivity.java
@@ -1,7 +1,10 @@
 package co.edu.udea.compumovil.gr6.lab3weather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import co.edu.udea.compumovil.gr6.lab3weather.service.Weather;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent t = new Intent(this, Weather.class);
+        t.putExtra("ciudad", "medellin");
+        this.startService(t);
     }
 }
