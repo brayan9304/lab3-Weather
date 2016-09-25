9d868235e476f0533248f4551c7824cace15fb85
jaime <jaime.jimenezisi@gmail.com>
2016-09-24 20:50:20 -0500

    agregar widget

---
+++ app/src/main/java/co/edu/udea/compumovil/gr6/lab3weather/widget/widgetProvider.java
@@ -0,0 +1,59 @@
+package co.edu.udea.compumovil.gr6.lab3weather.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
+
+import co.edu.udea.compumovil.gr6.lab3weather.R;
+
+/**
+ * Created by jaime on 24/09/2016.
+ */
+
+public class widgetProvider extends AppWidgetProvider {
+
+    private static final String ACTION_CLICK = "CLICK";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        //super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        ComponentName thisWidget = new ComponentName(context,widgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetId : allWidgetIds) {
+            // create some random data
+            Calendar fechaActual = Calendar.getInstance();
+            SimpleDateFormat fActual= new SimpleDateFormat("dd-MM-yyyy");
+            String fechaActualFormat= fActual.format(fechaActual.getTime());
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.widget_layout);
+            // Set the text
+            remoteViews.setTextViewText(R.id.Clima, "El Clima en "+"Medellin");
+            remoteViews.setTextViewText(R.id.fecha,fechaActualFormat);
+            remoteViews.setTextViewText(R.id.temperatura,"25 °C");
+            remoteViews.setTextViewText(R.id.humedad,"88%");
+
+            // Register an onClickListener
+            Intent intent = new Intent(context, widgetProvider.class);
+
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.Clima, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+
+    }
+}
